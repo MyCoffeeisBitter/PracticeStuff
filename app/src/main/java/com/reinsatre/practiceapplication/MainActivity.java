f436cd14f4bc46ca0e390701c340287b7cbfdd9d
BitterCoffee44 <tabanganjr0@gmail.com>
2023-08-04 18:36:43 +0800

    Practice App

---
+++ app/src/main/java/com/reinsatre/practiceapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.reinsatre.practiceapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.activity1change);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeActivity();
+            }
+        });
+    }
+
+    private void changeActivity(){
+        Intent intent = new Intent (this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/reinsatre/practiceapplication/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.reinsatre.practiceapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button changeActivity = findViewById(R.id.activity2button);
+
+        changeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeActivity();
+            }
+        });
+    }
+
+    private void changeActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
